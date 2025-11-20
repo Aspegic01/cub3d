c03837ed663b9264eca6602e1b833e163657450f
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-11-20 21:21:34 +0100

    refactor: optimize draw_line function for better precision and readability

--- engine/drawline.c
+++ engine/drawline.c
@@ -1,38 +1,34 @@
 #include "../cub3d.h"
 
-int32_t	maxabs_int32(int32_t a, int32_t b)
-{
-	int32_t	a1;
-	int32_t	b1;
-
-	a1 = abs(a);
-	b1 = abs(b);
-	if (a1 > b1)
-		return (a1);
-	return (b1);
-}
-
 void	draw_line(mlx_image_t *grid, t_v2 v1, t_v2 v2, uint32_t color)
 {
-	t_v2	delta;
-	t_v2	incr;
-	int32_t	steps;
+	double	delta_x;
+	double	delta_y;
+	double	steps;
+	double	incr_x;
+	double	incr_y;
+	double	x;
+	double	y;
 	int32_t	i;
 
-	delta = vec_sub(v2, v1);
-	steps = maxabs_int32(delta.x, delta.y);
+	delta_x = (double)(v2.x - v1.x);
+	delta_y = (double)(v2.y - v1.y);
+	steps = fmax(fabs(delta_x), fabs(delta_y));
 	if (steps == 0)
 	{
 		mlx_put_pixel(grid, v1.x, v1.y, color);
 		return ;
 	}
-	incr = vec_new(delta.x / steps, delta.y / steps);
+	incr_x = delta_x / steps;
+	incr_y = delta_y / steps;
+	x = (double)v1.x;
+	y = (double)v1.y;
 	i = 0;
-	while (i <= steps)
+	while (i <= (int32_t)steps)
 	{
-		mlx_put_pixel(grid, v1.x, v1.y, color);
-		v1.x += incr.x;
-		v1.y += incr.y;
+		mlx_put_pixel(grid, round(x), round(y), color);
+		x += incr_x;
+		y += incr_y;
 		i++;
 	}
 }
