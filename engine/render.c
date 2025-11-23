5cf565e58e9ff61f9d73b2408e7b4e001dba6af7
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-11-23 19:36:31 +0100

    feat: render floor and ceiling colors

--- engine/render.c
+++ engine/render.c
@@ -1,28 +1,57 @@
 #include "../cub3d.h"
 
-static int get_rgba(int r, int g, int b, int a)
+static int	get_rgba(int r, int g, int b, int a)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-void	render_game(t_game *game)
+void	render_floor(t_game *game)
+{
+	uint32_t	boundary;
+	uint32_t	x;
+	uint32_t	y;
+	uint32_t	c;
+
+	y = 0;
+	boundary = game->canvas->height / 2;
+	while (y < boundary)
+	{
+		x = 0;
+		while (x < game->canvas->width)
+		{
+			c = get_rgba(game->map->colors.ceiling_r,
+					game->map->colors.ceiling_g, game->map->colors.ceiling_b,
+					255);
+			mlx_put_pixel(game->canvas, x, y, c);
+			x++;
+		}
+		y++;
+	}
+}
+
+void	render_ceiling(t_game *game)
 {
 	uint32_t	x;
 	uint32_t	y;
-	t_colors	colors;
-	int	color;
+	uint32_t	c;
 
-	colors = game->map->colors;
-	y = 0;
+	y = game->canvas->height / 2;
 	while (y < game->canvas->height)
 	{
 		x = 0;
 		while (x < game->canvas->width)
 		{
-			color = get_rgba(colors.ceiling_r, colors.ceiling_g, colors.floor_b, 255);
-			mlx_put_pixel(game->canvas, x, y, color);
+			c = get_rgba(game->map->colors.floor_r, game->map->colors.floor_g,
+					game->map->colors.floor_b, 255);
+			mlx_put_pixel(game->canvas, x, y, c);
 			x++;
 		}
 		y++;
 	}
+}
+
+void	render_game(t_game *game)
+{
+	render_ceiling(game);
+	render_floor(game);
 }
