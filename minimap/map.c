52ceb62e1ad9c1943b2d45f401ee8fb6986abde3
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-11-24 18:56:42 +0100

    feat: pov rotation (WIP)

--- minimap/map.c
+++ minimap/map.c
@@ -1,9 +1,11 @@
 #include "../cub3d.h"
+#include <stdint.h>
 
-static void	render_map_cell(t_map *scene, t_v2i pos, uint32_t color, bool walkable)
+static void	render_map_cell(t_map *scene, t_v2i pos, uint32_t color,
+		bool walkable)
 {
-	t_v2i start;
-	t_v2i end;
+	t_v2i	start;
+	t_v2i	end;
 
 	start = veci_new(pos.x, pos.y);
 	end = veci_new(pos.x + CELL_SIZE, pos.y + CELL_SIZE);
@@ -12,7 +14,8 @@ static void	render_map_cell(t_map *scene, t_v2i pos, uint32_t color, bool walkab
 		start.x = pos.x;
 		while (start.x < end.x)
 		{
-			if (walkable && (end.y - start.y == CELL_SIZE|| end.x - start.x == CELL_SIZE))
+			if (walkable && (end.y - start.y == CELL_SIZE || end.x
+					- start.x == CELL_SIZE))
 				mlx_put_pixel(scene->img, start.x, start.y, 0x000000FF);
 			else
 				mlx_put_pixel(scene->img, start.x, start.y, color);
@@ -22,31 +25,46 @@ static void	render_map_cell(t_map *scene, t_v2i pos, uint32_t color, bool walkab
 	}
 }
 
-static void drawDirLine(t_map *scene, uint32_t color) {
-	t_v2f direction;
-	t_v2f center;
-	t_v2f pos;
+static void	draw_dirline(t_map *scene, uint32_t color)
+{
+	t_v2f	direction;
+	t_v2f	center;
 
-	pos = scene->player.position;
-	center = vecf_new(pos.x + PLAYER_HALF, pos.y + PLAYER_HALF);
+	center = vecf_new(scene->player.position.x + PLAYER_HALF,
+			scene->player.position.y + PLAYER_HALF);
 	direction = vecf_scale(scene->player.dir, CELL_SIZE);
 	draw_line(scene->img, center, vecf_add(center, direction), color);
 }
 
-// void draw_fov(t_map *scene, uint32_t color)
-// {
-// 	t_v2f pos = scene->player.position;
-// 	int32_t line_width = 4;
-// 	int32_t window_width = 16 * 100;
-// 	int32_t ray_count = window_width / line_width;
-// 	float_t rotationSpeed = 2 * (M_PI / 180);
-// 	float_t rotationAngle = scene->player.dir.x * rotationSpeed;
-// }
-
-static void	render_player(t_map *scene,  uint32_t color)
+void	draw_fov(t_map *scene, uint32_t color)
 {
-	t_v2i start;
-	t_v2i end;
+	int32_t	ray_count;
+	// float_t	rotation_angle;
+	// float_t	ray_angle;
+	t_v2f	ray_dir;
+	int32_t	i;
+
+	ray_count = scene->width / 4;
+	// rotation_angle = scene->player.dir.x * ROT_SPEED;
+	// ray_angle = rotation_angle - (FOV_ANGLE / 2);
+	i = 0;
+
+	t_v2f center = vecf_new(scene->player.position.x + PLAYER_HALF,
+			scene->player.position.y + PLAYER_HALF);
+	while (i < ray_count)
+	{
+		// ray_dir = vecf_new(center.x + cosf(ray_angle) * 30, center.y + sinf(ray_angle) * 30);
+		ray_dir = vecf_scale(scene->player.dir, CELL_SIZE);
+		draw_line(scene->img, center, vecf_add(center, ray_dir), color);
+		// ray_angle += FOV_ANGLE / ray_count;
+		i++;
+	}
+}
+
+static void	render_player(t_map *scene, uint32_t color)
+{
+	t_v2i	start;
+	t_v2i	end;
 
 	start = veci_new(scene->player.position.x, scene->player.position.y);
 	end = veci_new(start.x + PLAYER_SIZE, start.y + PLAYER_SIZE);
@@ -60,16 +78,15 @@ static void	render_player(t_map *scene,  uint32_t color)
 		}
 		start.y++;
 	}
-
-	drawDirLine(scene, color);
-	// draw_fov(scene, color);
+	draw_fov(scene, color);
+	draw_dirline(scene, color);
 }
 
 void	minimap_render(t_map *scene)
 {
-	t_v2i pos;
-	int line_len;
-	
+	t_v2i	pos;
+	int		line_len;
+
 	pos = veci_zero();
 	while (pos.y < scene->height)
 	{
@@ -78,21 +95,22 @@ void	minimap_render(t_map *scene)
 		while (pos.x < scene->width)
 		{
 			if (pos.x < line_len && scene->grid[pos.y][pos.x] == '0')
-				render_map_cell(scene, veci_scale(pos, CELL_SIZE), 0xFFFFFFFF, true);
+				render_map_cell(scene, veci_scale(pos, CELL_SIZE), 0xFFFFFFFF,
+					true);
 			else
-				render_map_cell(scene, veci_scale(pos, CELL_SIZE), 0x333333FF, false);
+				render_map_cell(scene, veci_scale(pos, CELL_SIZE), 0x333333FF,
+					false);
 			pos.x++;
 		}
 		pos.y++;
 	}
-
 	render_player(scene, 0x0000FFFF);
 }
 
 static void	map_print(t_map *map)
 {
-	size_t i;
-	size_t j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	while (map->grid[i])
@@ -118,8 +136,10 @@ int	minimap_setup(t_game *game)
 	if (!game->map->img)
 		return (ft_putstr_fd("Error\nFailed to initialize image\n", 2), -1);
 	game->map->position = veci_new(WIN_WIDTH * 0.01, WIN_HEIGHT * 0.01);
-	game->map->player.position = vecf_scale(game->map->player.position, CELL_SIZE);
-	img_idx = mlx_image_to_window(game->mlx, game->map->img, game->map->position.x, game->map->position.y);
+	game->map->player.position = vecf_scale(game->map->player.position,
+			CELL_SIZE);
+	img_idx = mlx_image_to_window(game->mlx, game->map->img,
+			game->map->position.x, game->map->position.y);
 	if (img_idx < 0)
 		return (ft_putstr_fd("Error\nFailed to put image to window\n", 2), -1);
 	map_print(game->map);
